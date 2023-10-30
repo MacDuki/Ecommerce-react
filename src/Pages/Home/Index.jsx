e81aeac39d3469f7e058734799314e70604af84b
MacDuki <PCMYACT@hotmail.com>
2023-10-29 23:12:07 -0300

    Arreglo de showmaximized

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { shopContext } from "../../Context/Index";
 import { Layout } from "../../Layout/Index";
 import { MaximizedProduct } from "../../MaximizedProduct/Index";
 import { MinimizedProduct } from "../../MinimizedProduct";
+
 import "./Home.css";
 
 function Home() {
 	const [products, setProducts] = useState(null);
-
+	const { showMaximized } = React.useContext(shopContext);
 	useEffect(() => {
 		fetch("https://fakestoreapi.com/products").then((response) =>
 			response.json().then((data) => setProducts(data))
@@ -15,12 +17,15 @@ function Home() {
 
 	return (
 		<Layout>
-			<div className='home-container'>
-				{products?.map((product) => (
-					<MinimizedProduct key={product.id} product={product} />
-				))}
+			{showMaximized ? (
 				<MaximizedProduct />
-			</div>
+			) : (
+				<div className='home-container'>
+					{products?.map((product) => (
+						<MinimizedProduct key={product.id} product={product} />
+					))}
+				</div>
+			)}
 		</Layout>
 	);
 }

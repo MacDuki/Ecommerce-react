7cf077a4ef81c575ba4a5fcac8aa159d586d49fc
MacDuki <PCMYACT@hotmail.com>
2023-11-06 23:49:36 -0300

    Agrego funcionalidad de categorías a inicio

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,22 +1,45 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { shopContext } from "../../Context/Index";
 import { Layout } from "../../Layout/Index";
 import "./Home.css";
 function Home() {
+	const { setCategory } = React.useContext(shopContext);
 	return (
 		<>
 			<Layout>
 				<section className='welcome-section'>
-					<div>
-						<h1>Bienvenido a GARAGE SHOP </h1>
-						<div className='img-container'>
-							<div>
-								<img src='src\assets\img\clothing-model.jpeg' />
-							</div>
-							<div>
-								<img src='src\assets\img\electronics-model.jpg' />
-							</div>
-							<div>
-								<img src='src\assets\img\jewelery-model.jpeg' />
-							</div>
+					<h1>Bienvenido a GARAGE SHOP </h1>
+					<div className='img-container'>
+						<div>
+							<NavLink to='/clothing'>
+								<img
+									onClick={() => {
+										setCategory("clothing");
+									}}
+									src='src\assets\img\clothing-model.jpeg'
+								/>
+							</NavLink>
+						</div>
+						<div>
+							<NavLink to='/electronics'>
+								<img
+									onClick={() => {
+										setCategory("electronics");
+									}}
+									src='src\assets\img\electronics-model.jpg'
+								/>
+							</NavLink>
+						</div>
+						<div>
+							<NavLink to='/jewelery'>
+								<img
+									onClick={() => {
+										setCategory("jewelery");
+									}}
+									src='src\assets\img\jewelery-model.jpeg'
+								/>
+							</NavLink>
 						</div>
 					</div>
 				</section>

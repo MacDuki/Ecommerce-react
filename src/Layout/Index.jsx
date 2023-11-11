4d22d70343b1d87fd05d012147c3e47fc0598cd3
MacDuki <PCMYACT@hotmail.com>
2023-11-10 23:42:07 -0300

    Comienzo a modelar footer

--- src/Layout/Index.jsx
+++ src/Layout/Index.jsx
@@ -5,7 +5,16 @@ import "./layout.css";
 function Layout({ children }) {
 	const { cssClassHomeLayout } = useContext(shopContext);
 
-	return <main className={cssClassHomeLayout + " layout"}>{children}</main>;
+	return (
+		<main className={cssClassHomeLayout + " layout"}>
+			{children}
+			<footer className='footer'>
+				<p className='info-p'>sdsddsdsddsdsds </p>
+				<p className='mention1-p'> dsdssdsddsd </p>
+				<p className='mention2-p'>dsdsd ssdsd</p>
+			</footer>
+		</main>
+	);
 }
 
 export { Layout };
